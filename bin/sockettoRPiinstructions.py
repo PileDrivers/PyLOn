da6f3009989a5af16e21a4ad97e9a0cd777569c5
eyesniper2 <eyesniper2@rayfall.net>
2018-01-28 03:31:42 -0700

    "Geez Rick, I had to update the site because of all these deploys."

--- bin/sockettoRPiinstructions.py
+++ bin/sockettoRPiinstructions.py
@@ -7,15 +7,15 @@ from socketIO_client import SocketIO, BaseNamespace
 # logging.getLogger('requests').setLevel(logging.WARNING)
 # logging.basicConfig(level=logging.DEBUG)
 
-FETChannels = [7,11] # Left motor is pin 7, right motor is pin 11
+# FETChannels = [7,11] # Left motor is pin 7, right motor is pin 11
 
-# ser = serial.Serial(
-#     port='COM6',
-#     baudrate=9600,
-#     parity=serial.PARITY_ODD,
-#     stopbits=serial.STOPBITS_TWO,
-#     bytesize=serial.SEVENBITS
-# )
+ser = serial.Serial(
+    port='COM6',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
 
 class PylonNamespace(BaseNamespace):
     
@@ -28,18 +28,18 @@ class PylonNamespace(BaseNamespace):
         command = args[1]['command']
         #print(command)
         if command == "left":
-            ser.write('1'.encode('utf-8'))
-        elif command == "right":
             ser.write('2'.encode('utf-8'))
-        elif command == "forward":
+        elif command == "right":
             ser.write('3'.encode('utf-8'))
+        elif command == "forward":
+            ser.write('1'.encode('utf-8'))
 
 def main():
    
     print('run check') #/dev/ttyUSB1
-    #ser.isOpen(); print('Serial Open')
+    ser.isOpen(); print('Serial Open')
 
-    socketIO = SocketIO('https://pylon-voting-server.herokuapp.com', verify=False)
+    socketIO = SocketIO('https://pylon-driver-101.herokuapp.com/', verify=False)
     #socketIO.wait(seconds=1)
     pylon_namespace = socketIO.define(PylonNamespace, '/pylon')
     socketIO.wait()
